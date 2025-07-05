292011943cdbaaa1d92048bf0707e2b378d0041c
Dhurkesh-B <dhurkeshmyself@gmail.com>
2025-07-05 09:45:29 +0530

    updated on 5.7.2025

---
+++ 1394/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i:arr)
+            map.put(i,map.getOrDefault(i,0)+1);
+        int res = -1;
+        for(int i:map.keySet()){
+            if(i==map.get(i))
+                res = Math.max(i,res);   
+        }
+        return res;
+       
+    }
+}
