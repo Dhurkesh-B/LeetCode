5e49a7f56e27da0a40b0ff45166e25b36a164c9b
Dhurkesh-B <dhurkeshmyself@gmail.com>
2025-10-02 06:56:54 +0530

    Updated on 02.10.2025

--- 3100/solution.java
+++ 3100/solution.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
         int result = numBottles;
         int empty = numBottles;
-        int fullBottles = 0;
         while(empty>=numExchange){
             empty-=(numExchange-1);
             numExchange++;
