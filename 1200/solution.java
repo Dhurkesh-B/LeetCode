bc4dc49c902287f4e66330c18a7ee4cbf90b0343
Dhurkesh B <dhurkeshmyself@gmail.com>
2026-01-26 16:16:14 +0530

    Updated 26.01.2026

---
+++ 1200/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+  public List<List<Integer>> minimumAbsDifference(int[] arr) {
+    int n = arr.length;
+    Arrays.sort(arr);
+    int minDiff = Integer.MAX_VALUE;
+    for (int i = 1; i < n; i++)
+      minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+    List<List<Integer>> result = new ArrayList<>();
+    for (int i = 1; i < n; i++) {
+      if (arr[i] - arr[i - 1] == minDiff)
+        result.add(Arrays.asList(arr[i - 1], arr[i]));
+    }
+    return result;
+  }
+}
