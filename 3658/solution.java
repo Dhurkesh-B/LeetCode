0d0fc904bcc8c7f1b9f0f757703bce4c11408ab2
Dhurkesh <dhurkeshmyself@gmail.com>
2026-07-15 14:45:15 +0530

    Updated on 15.7.26

---
+++ 3658/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int gcd(int a, int b){
+        while(b!=0){
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+    public int gcdOfOddEvenSums(int n) {
+        int evenSum = n*(n+1);
+        int oddSum = n*n;
+        return gcd(oddSum, evenSum);
+    }
+}
---
+++ 3658/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        oddSum = n*n 
+        evenSum = n*(n+1)
+        return math.gcd(oddSum, evenSum)
