543d4d5c92abd21b5fada2eb27f1a42439f6d6c8
Dhurkesh <dhurkeshmyself@gmail.com>
2026-07-21 21:55:00 +0530

    Updated on 21.7.26

--- 3499/solution.java
+++ 3499/solution.java
@@ -1,26 +1,24 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
         int ones = 0;
+        int prev = Integer.MIN_VALUE;
         int curr = 0;
         int res = 0;
-        List<Integer> zeros = new ArrayList<>();
-        
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='0')
                 curr++;
             else{
                 if(curr>0){
-                    zeros.add(curr);
-                    curr = 0;   
+                    res = Math.max(res, curr+prev);
+                    prev = curr;
+                    curr = 0;
                 }
                 ones++;
             }
         }
         if(curr>0)
-            zeros.add(curr);
-        
-        for(int i=1;i<zeros.size();i++)
-            res = Math.max(res, zeros.get(i-1)+zeros.get(i));
-        return res+ones;
+            res = Math.max(res, curr+prev);
+        res+=ones;        
+        return res;
     }
 }
--- 3499/solution.py
+++ 3499/solution.py
@@ -1,19 +1,20 @@
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        ones = s.count('1')
+        ones = 0
         curr = 0
+        prev = float('-inf')
         res = 0
-        zeros = []
         for i in s:
             if i=='0':
                 curr+=1
-            elif curr:
-                zeros.append(curr)
-                curr = 0 
+            else:
+                if curr:
+                    res = max(res, curr+prev)
+                    prev = curr 
+                    curr = 0
+                ones+=1    
         if curr:
-            zeros.append(curr)
-            
-        for i in range(1,len(zeros)):
-            res = max(res, zeros[i-1]+zeros[i])
-
-        return res+ones
+            res = max(res, curr+prev)
+        
+        res+=ones
+        return res
